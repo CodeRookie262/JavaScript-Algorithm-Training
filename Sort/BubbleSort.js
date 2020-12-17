ece4b1b826d06ab599034ab727f2c21f8b16afcb
CodeRookie262 <2269082753@qq.com>
2020-12-17 18:20:29 +0800

    ✨feat(Sort): 增加插入排序

--- Sort/BubbleSort.js
+++ Sort/BubbleSort.js
@@ -10,6 +10,11 @@ const arr = getArray(10, 10);
  * 空间复杂度： O(1)
  * 时间复杂度：O(n^2)
  * 属于原地排序，稳定
+ *
+ * Description：
+ *  冒泡排序通过比较相邻的元素进行排序的，如果当 前元素大于下一个元素 则交换位置，第一轮走完后最后一个必定是设置中最大的值，直到数组没有发生过排序则跳出循环体终止循环。
+ *  Image： bubbleSort.gif (＾－＾)V
+ * （我有一个想法，那它岂不是可以找 第K值 ）
  */
 
 const BubbleSort = function BubbleSort(array) {
---
+++ Sort/InsertionSort.js
@@ -0,0 +1,44 @@
+const { getArray, logger } = require('../utils');
+// logger();
+console.log('============Insertion Sort=============');
+
+const arr = getArray(10, 10);
+
+/**
+ * 插入排序
+ *
+ * 空间复杂度： O(1)
+ * 时间复杂度：O(n^2)
+ * 属于原地排序，稳定
+ *
+ * Image: InsertionSort.gif
+ *
+ * Description：
+ *  插入排序会讲数组分为两个区间，分别是排序区间和未排序区间，插入排序会从未排序区间中逐一取元素与排序区间进行排序。
+ */
+const InsertionSort = function InsertionSort(array) {
+  const length = array.length;
+
+  if (length <= 1) return array;
+
+  for (var i = 1; i < length; i++) {
+    let value = array[i];
+    for (var j = i - 1; j >= 0; j--) {
+      if (array[j] > value) {
+        array[j + 1] = array[j];
+      } else {
+        // 因为左边是排序区间，如果与最后的元素已经处于有序的话则说明该元素不用在单轮循环与其他元素进行排序了。
+        break;
+      }
+    }
+    array[j + 1] = value;
+  }
+
+  return array;
+};
+
+console.log(`
+  Sort array is: ${JSON.stringify(InsertionSort(arr))}
+`);
+
+Promise;
